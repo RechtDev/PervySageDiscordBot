0039f2e7cf5a4b48af4b011eb04499d12712848a
RechtDev <domjr452@hotmail.com>
2021-09-12 01:55:02 -0400

    Secure Token Access Added securer way to get token not displayed to public

--- PervySageBot/Program.cs
+++ PervySageBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus;
 using PervySageBot.Content.Fetcher;
 using System;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -14,9 +15,18 @@ namespace PervySageBot
         }
         static async Task MainAsync()
         {
+            string token;
+            if(File.Exists(@"C:\MyTestKeyPair\Tokens\PSTok.txt"))
+            {
+                token = File.ReadAllText(@"C:\MyTestKeyPair\Tokens\PSTok.txt");
+            }
+            else
+            {
+                token = "error";
+            }
             var disClient = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "ODg1ODQ2MTM3NzMzNTQ2MDA1.YTs-gQ.iBztyDAXyZnt7K0xS21N7ALMB-A",
+                Token = token,
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged
                 
